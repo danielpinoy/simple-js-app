cce13e57be599492ad72988ae14997dffeeefd9c
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2023-09-15 14:16:33 +0800

    added pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,13 @@
 
 // document.write(favoriteFood);
 // alert("Hello World");
+
+// Task 2
+
+let pokemonList = [
+    { name: "Fearow", height: 1.2, types: ["normal", "flying"] },
+    { name: "Pikachu", height: 0.4, types: ["electric"] },
+    { name: "Sunflora", height: 0.8, types: ["grass"] },
+    { name: "Kadabra", height: 1.3, types: ["psychic"] },
+    { name: "Emboar", height: 1.6, types: ["fire", "fighting"] },
+];
