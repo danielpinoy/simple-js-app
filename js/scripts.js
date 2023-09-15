2d4992aa2c406bdcd92250d39ae0e0fe72b1e333
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2023-09-15 11:18:47 +0800

    added javascript code

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/* 
+let myName = "Daniel John";
+document.write(myName);
+myName = "John Doe";
+document.write(myName);
+*/
+
+let favoriteFood = "Pizza";
+
+document.write(favoriteFood);
+alert("Hello World");
