bbb545b2c15d99d64e6b0a9df347b37af724763c
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2023-09-16 13:08:11 +0800

    added task 3
    for loop an array of objects with else if condition

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@
 
 // Task 2
 
+// array of objects containing pokemon
 let pokemonList = [
     { name: "Fearow", height: 1.2, types: ["normal", "flying"] },
     { name: "Pikachu", height: 0.4, types: ["electric"] },
@@ -14,3 +15,24 @@ let pokemonList = [
     { name: "Kadabra", height: 1.3, types: ["psychic"] },
     { name: "Emboar", height: 1.6, types: ["fire", "fighting"] },
 ];
+
+// Task 3
+
+// for loop
+// loops around the pokemonList array
+for (let i = 0; i < pokemonList.length; i++) {
+    // condition to find the pokemon with an average height
+    if (pokemonList[i].height > 0.4 && pokemonList[i].height < 0.9) {
+        document.write(
+            `${pokemonList[i].name} Height (${pokemonList[i].height}) Average Height <br>`
+        );
+    }
+    //  find the tallest pokemon
+    else if (pokemonList[i].height > 1) {
+        document.write(`${pokemonList[i].name} Height (${pokemonList[i].height}) Tall  <br>`);
+    }
+    // find the shortest pokemon
+    else {
+        document.write(`${pokemonList[i].name} Height (${pokemonList[i].height}) Small <br>`);
+    }
+}
