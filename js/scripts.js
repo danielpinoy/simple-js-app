19a185225918e9c84f59fbedc48755daa47e8c8a
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2023-09-18 14:43:18 +0800

    added task 4
    -added IIFE with functions
    -added foreach to loop around the pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,48 @@
 // array of objects containing pokemon
-let pokemonList = [
-    { name: "Fearow", height: 1.2, types: ["normal", "flying"] },
-    { name: "Pikachu", height: 0.4, types: ["electric"] },
-    { name: "Sunflora", height: 0.8, types: ["grass"] },
-    { name: "Kadabra", height: 1.3, types: ["psychic"] },
-    { name: "Emboar", height: 1.6, types: ["fire", "fighting"] },
-];
+
+// Task 4
+let pokemonRepository = (() => {
+    let pokemonList = [
+        { name: "Fearow", height: 1.2, types: ["normal", "flying"] },
+        { name: "Pikachu", height: 0.4, types: ["electric"] },
+        { name: "Sunflora", height: 0.8, types: ["grass"] },
+        { name: "Kadabra", height: 1.3, types: ["psychic"] },
+        { name: "Emboar", height: 1.6, types: ["fire", "fighting"] },
+    ];
+    return {
+        getAll: function () {
+            return pokemonList;
+        },
+        add: function (pokemon) {
+            return pokemonList.push(pokemon);
+        },
+    };
+})();
+
+pokemonRepository.getAll().forEach((pokemon) => {
+    let pokemonName = `${pokemon.name} Height (${pokemon.height})`;
+
+    // condition to find the pokemon with an average height
+    if (pokemon.height > 0.4 && pokemon.height < 0.9) {
+        document.write(`${pokemonName} - Average  <br>`);
+    }
+
+    //  find the tallest pokemon
+    else if (pokemon.height > 1) {
+        document.write(`${pokemonName} - Tall  <br>`);
+    }
+
+    // filter out the shortest
+    else {
+        document.write(`${pokemonName} - Small <br>`);
+    }
+});
 
 // Task 3
 
 // for loop
 // loops inside the pokemonList array of objects
+
 for (let i = 0; i < pokemonList.length; i++) {
     let pokemonName = `${pokemonList[i].name} Height (${pokemonList[i].height})`;
 
